ad35e6e38435a54154f9359822d7d5fa41f58b10
wassi244255@gmail.com <wassi244255@gmail.com>
2024-11-21 21:39:18 +0100

    initial str + DB connection working

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // Update with your backend URL
+  baseURL: 'http://127.0.0.1:5000', // Update with your backend URL
 });
 
 export default axiosInstance;
